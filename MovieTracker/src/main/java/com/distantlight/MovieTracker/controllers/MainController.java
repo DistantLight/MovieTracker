b6273e678957acfc0efea200b1557e140c33a165
DistantLight <dist4ntlight@gmail.com>
2023-10-13 02:09:01 +0300

    add object for request for movie filter

--- MovieTracker/src/main/java/com/distantlight/MovieTracker/controllers/MainController.java
+++ MovieTracker/src/main/java/com/distantlight/MovieTracker/controllers/MainController.java
@@ -12,19 +12,9 @@ import java.util.stream.Collectors;
 
 @RestController
 public class MainController {
-/*    @PostMapping("/filter")
-    public ResponseEntity<?> getMainPage(@RequestBody MovieFilterRequest movieFilterRequest){
-
-        ArrayList<String> movieList = new ArrayList<>(List.of(movieFilterRequest.getText().split("\\n")));
-        movieList = movieList
-                .stream()
-                .map(line -> line.replace("\\n", ""))
-                .collect(Collectors.toCollection(ArrayList::new));
-        return ResponseEntity.ok(movieList);
-    }*/
-
     @PostMapping("/filter")
-    public ResponseEntity<?> getMainPage(@RequestBody String string){
-        return ResponseEntity.ok(string);
+    public ResponseEntity<?> getMainPage(@RequestBody MovieFilterRequest movieFilterRequest){
+        ArrayList<String> movieList = new ArrayList<>(List.of(movieFilterRequest.getText().split("\\n")));
+        return ResponseEntity.ok(movieList);
     }
 }

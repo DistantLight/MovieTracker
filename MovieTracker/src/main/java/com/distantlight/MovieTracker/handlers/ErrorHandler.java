75af79f4ff46edd2790db6272c7d19ba9d447677
DistantLight <dist4ntlight@gmail.com>
2023-10-18 19:10:47 +0300

    add exception handling

---
+++ MovieTracker/src/main/java/com/distantlight/MovieTracker/handlers/ErrorHandler.java
@@ -0,0 +1,30 @@
+package com.distantlight.MovieTracker.handlers;
+
+import jakarta.annotation.Priority;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.ResourceAccessException;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.ConnectException;
+
+@ControllerAdvice
+public class ErrorHandler {
+    @ExceptionHandler(ResourceAccessException.class)
+    public ResponseEntity<?> handleConnectException() {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Нет соединения со сторонним сервисом");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleException(Exception e) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Произошла ошибка на сервере");
+    }
+}
